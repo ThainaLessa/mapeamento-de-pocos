beb412cb7c6d13b59c457df38df1d18eab4d9585
Thainá Lessa <thaina_lessa@hotmail.com>
2018-05-12 12:42:32 -0300

    Módulo para leitura das coordenadas dos poços e transformação para graus decimais

--- Código/codigo.py
+++ Código/codigo.py
@@ -22,7 +22,20 @@ def dms_to_dd(d, m, s):
     dd = d + float(m)/60 + float(s)/3600
     return dd
 def manipular_coordenadas():
-    
+    df = pd.read_excel('coordenadas.xlsx', 0, header=0)
+
+    for i in range(len(df['Longitude'])):
+        lat = df['Latitude'][i]
+        lon = df['Longitude'][i]
+        #Uniformizando a escrita das coordenadas:
+        g_lat, m_lat, s_lat = lat.replace('°',' ').replace("''",'"').replace("'",' ').replace('"','').split(' ')
+        g_lon, m_lon, s_lon = lon.replace('°',' ').replace("''",'"').replace("'",' ').replace('"','').split(' ')
+        #print(lat)
+
+        #Mudar de coordenadas geográficas grau, minuto, segundo para grau decimal (*(-1) para representar Sul e Oeste):
+        df['Latitude'][i] = dms_to_dd(int(g_lat),int(m_lat),float(s_lat))*(-1)
+        df['Longitude'][i] = dms_to_dd(int(g_lon),int(m_lon),float(s_lon))*(-1)
+    return(df)
 
 '''
 Cálculos e análises
