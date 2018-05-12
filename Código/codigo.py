3ada80bcd5af7ba151870d4fb218830484923e2c
Thainá Lessa <thaina_lessa@hotmail.com>
2018-05-12 12:39:41 -0300

    Módulo para classificação da água por parâmetro (incompleto)

--- Código/codigo.py
+++ Código/codigo.py
@@ -33,6 +33,37 @@ def calc_stats(dados_poços):
 
 #Classificar a água dos poços conforme CONAMA 396/2008 e Porria do MS 518/2004:
 def classif_agua(stats, dados_poços):
+    df_poços = pd.DataFrame(dados_poços, columns=list(dados_poços.keys()))
+    df_padrões = pd.read_excel('padrões de qualidade.xlsx',0,header=1,index_col=0)
+    df_vrq = pd.read_excel('VRQ - poços.xlsx',0,header=1,index_col=0)
+
+    VMPr_mais = {}
+    VMPr_menos = {}
+    for param in list(df_padrões.columns):
+        '''
+        Encontrar, entre os padrões de qualidade, o valor máximo permitido mais restritivo (VMPr+)
+        e o menos restritivo (VMPr-)
+        '''
+        VMPr_mais[param] = df_padrões[param].min()
+        VMPr_menos[param] = df_padrões[param].max()
+    
+    #Fazer análise do parâmetro para classificar:
+    '''
+    VERIFICAR EXISTÊNCIA DE COLIFORMES E/OU NITRATO PARA CLASSIFICAR COMO CLASSE 3/4 
+    Alterar --> Thainá 
+    '''
+    for poço in list(df_poços.columns):
+        for param in list(df_poços[poço].index):
+            if  stats[poço][param]['3º quartil'] <= VMPr_mais[param]:
+                if df_vrq[param][poço] <= VMPr_mais[param]:
+                    print(poço+param+'Classe 1')
+                else:
+                    print(poço+param+'Classe 2')
+            else:
+                if df_vrq[param][poço] > VMPr_mais[param] and stats[poço][param]['3º quartil'] <= df_vrq[param][poço]:
+                    print(poço+param+'Classe 3')
+                elif df_vrq[param][poço] > VMPr_menos[param] and stats[poço][param]['3º quartil'] <= df_vrq[param][poço]:
+                    print(poço+param+'Classe 4')
 
 '''
 Criar banco de dados --> Thainá
