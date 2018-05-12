390014fe71bcf143a7df47b37526336133835421
Thainá Lessa <thaina_lessa@hotmail.com>
2018-05-12 12:46:32 -0300

    Módulo para cálculo das estatísticas

--- Código/codigo.py
+++ Código/codigo.py
@@ -30,7 +30,6 @@ def manipular_coordenadas():
         #Uniformizando a escrita das coordenadas:
         g_lat, m_lat, s_lat = lat.replace('°',' ').replace("''",'"').replace("'",' ').replace('"','').split(' ')
         g_lon, m_lon, s_lon = lon.replace('°',' ').replace("''",'"').replace("'",' ').replace('"','').split(' ')
-        #print(lat)
 
         #Mudar de coordenadas geográficas grau, minuto, segundo para grau decimal (*(-1) para representar Sul e Oeste):
         df['Latitude'][i] = dms_to_dd(int(g_lat),int(m_lat),float(s_lat))*(-1)
@@ -42,7 +41,23 @@ Cálculos e análises
 '''
 #Cálculo das estatísticas: mínimo, máximo, média, mediana e 3º quartil:
 def calc_stats(dados_poços):
-    
+    df = pd.DataFrame(dados_poços, columns=list(dados_poços.keys()))
+    stats = {}
+
+    for poço in list(df.columns):
+        stats[poço] = {}
+        for param in list(df[poço].index):
+            stats[poço][param] = {}
+            #Trocar valor não numérico (string no nosso caso) por NaN para poder realizar os cálculos posteriormente:
+            df[poço][param] = pd.to_numeric(df[poço][param], errors='coerce')
+            
+            #Cálculo das estatísticas:
+            stats[poço][param]['Máximo'] = df[poço][param].drop(df[poço][param].index[0]).max()
+            stats[poço][param]['Mínimo'] = df[poço][param].drop(df[poço][param].index[0]).min()
+            stats[poço][param]['Média'] = df[poço][param].drop(df[poço][param].index[0]).mean()
+            stats[poço][param]['Mediana'] = df[poço][param].drop(df[poço][param].index[0]).median()
+            stats[poço][param]['3º quartil'] = df[poço][param].drop(df[poço][param].index[0]).sort_values().quantile(q=0.75, interpolation='midpoint')
+    return stats
 
 #Classificar a água dos poços conforme CONAMA 396/2008 e Porria do MS 518/2004:
 def classif_agua(stats, dados_poços):
